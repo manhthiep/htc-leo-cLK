ea15405e8556befc9d85d4ce5ea639214455fcdd
Arif Ali <mail@arif-ali.co.uk>
2011-11-06 23:25:14 +0000

    1.1: Initial commit

--- lk/app/aboot/rules.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_DIR := $(GET_LOCAL_DIR)
-
-OBJS += \
-	$(LOCAL_DIR)/aboot.o \
-	$(LOCAL_DIR)/fastboot.o \
-	$(LOCAL_DIR)/recovery.o
-
--- lk/include/version.h
+++
@@ -1,7 +0,0 @@
-#ifndef __VERSION_H
-#define __VERSION_H
-
-static const char *cLK_version = "1.4.0.1";
-
-#endif
-
